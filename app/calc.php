440c23d3627a6e3cc91ca87acbb7057d0b4111b0
JakubBalda <balda.jakub00@gmail.com>
2022-03-02 17:01:18 +0100

    poprawka zabezpieczeń

--- app/calc.php
+++ app/calc.php
@@ -9,11 +9,19 @@ $opr = $_REQUEST ['opr'];
 
 // nie potrzebna jest walidacja czy zostały uzupełnione pola ze wzglądu na atrybut 'required' w znacznikach input
 	if (!is_numeric($kwota)) {
-		$messages [] = 'Kwota musi być liczbą';
+		$messages [] = 'Kwota musi być liczbą!';
 	}
 	
 	if (!is_numeric($lata)) {
-		$messages [] = 'Ilość lat musi być liczbą';
+		$messages [] = 'Ilość lat musi być liczbą!';
+	}
+	if($lata <= 0)
+	{
+		$messages [] = 'Błędna liczba lat!';
+	}
+	if($kwota <=0)
+	{
+		$messages [] = 'Błędne podana kwota';
 	}	
 
 if (empty($messages)) { 
