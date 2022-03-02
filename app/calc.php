f377a2deea7b482988621c9f14d94abf81be6fd5
Jakub Balda <kuber579@gmail.com>
2022-03-02 10:53:42 +0100

    git commit

---
+++ app/calc.php
@@ -0,0 +1,35 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+
+
+$kwota = $_REQUEST ['kwota'];
+$lata = $_REQUEST ['lata'];
+$opr = $_REQUEST ['opr'];
+
+
+
+if (empty($messages)) {
+	
+	if (!is_numeric($kwota)) {
+		$messages [] = 'Kwota musi być liczbą';
+	}
+	
+	if (!is_numeric($lata)) {
+		$messages [] = 'Ilość lat mus być liczbą';
+	}	
+
+}
+
+if (empty($messages)) { 
+
+	$kwota = floatval($kwota);
+	$lata = intval($lata);
+    $opr = intval($opr);
+	
+    If($opr != 0)
+        $rate = ($kwota + ($kwota *($opr/100) ))/($lata*12);
+    else
+        $rate = $kwota/($lata*12); 
+}
+
+include 'calc_view.php';
---
+++ app/calc_view.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator kredytowy</title>
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<label for="id_kwota">Kwota: </label>
+    <input type="text" id="id_kwota" name="kwota" required/><br />
+    <label for="id_lata">Lata: </label>
+    <input type="text" id="id_lata" name="lata" required/><br />
+    <label for="id_opr">Oprocentowanie: </label>
+    <select name="opr">
+        <option value="0">0%</option>
+        <option value="5">5%</option>
+        <option value="10">10%</option>
+        <option value="15">15%</option>
+        <option value="20">20%</option>
+    </select><br />
+	<input type="submit" value="Oblicz" />
+</form>	
+
+<?php
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px;  background-color: #b85699; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($rate)){ ?>
+<div style="margin: 20px; padding: 10px;  background-color: #021548;color: white; width:300px;">
+<?php echo 'Wynik: '.$rate; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
