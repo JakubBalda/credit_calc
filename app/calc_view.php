c270528d7c25cf5d8155dfd44494a98be35e32f0
JakubBalda <balda.jakub00@gmail.com>
2022-03-19 13:01:01 +0100

    adding frontend 1.2

--- app/calc_view.php
+++ app/calc_view.php
@@ -1,45 +1,68 @@
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
-<meta charset="utf-8" />
-<title>Kalkulator kredytowy</title>
+    <meta charset="utf-8" />
+    <title>Kalkulator kredytowy</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" /> 
 </head>
 <body>
 
-<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
-	<label for="id_kwota">Kwota: </label>
-    <input type="text" id="id_kwota" name="kwota" required value="<?php if(isset($kwota)) print($kwota);?>"/><br />
-    <label for="id_lata">Lata: </label>
-    <input type="text" id="id_lata" name="lata" required value="<?php if(isset($lata)) print($lata);?>"/><br />
-    <label for="id_opr">Oprocentowanie: </label>
-    <select name="opr">
-        <option value="0">0%</option>
-        <option value="5">5%</option>
-        <option value="10">10%</option>
-        <option value="15">15%</option>
-        <option value="20">20%</option>
-    </select><br />
-	<input type="submit" value="Oblicz" />
-</form>	
-<a href="<?php print(_APP_ROOT);?>/app/security/logout.php">Wyloguj się</a>
-<?php
-if (isset($messages)) {
-	if (count($messages) > 0) {
-		echo '<ul style="margin: 20px; padding: 10px 10px 10px 30px; color: #fff; background-color: #860500; width:300px;">';
-        echo 'Błędnie uzupełniony formularz!!! <br />';
-		foreach ($messages as $key => $msg) {
-			echo '<li>'.$msg.'</li>';
-		}
-		echo '</ul>';
-	}
-}
-?>
+    <div class="container" id="container" >
+        <div class="form-container sign-in-container">
 
-<?php if (isset($rate)){ ?>
-<div style="margin: 20px; padding: 10px;  background-color: #021548;color: white; width:300px;">
-<?php echo 'Miesięczna rata: '.round($rate, 2).' zł'; ?>
-</div>
-<?php } ?>
+            <form action="<?php print(_APP_URL);?>/app/calc.php" method="post" class="form" id="login">
+                <h1 class="form__title">Kalkulator kredytowy</h1>
+                <fieldset>
+                    <div class="form__title-group">
+                        <label for="id_kwota">Kwota: </label>
+                        <input type="text" id="id_kwota" name="kwota" required value="<?php if(isset($kwota)) print($kwota);?>"/>
+                    </div>
+                    <div class="form__title-group">
+                        <label for="id_lata">Lata: </label>
+                        <input type="text" id="id_lata" name="lata" required value="<?php if(isset($lata)) print($lata);?>"/>
+                    </div>
+                    <div class="form__title-group">
+                        <label for="id_opr">Oprocentowanie: </label>
+                        <select name="opr">
+                            <option value="0">0%</option>
+                            <option value="5">5%</option>
+                            <option value="10">10%</option>
+                            <option value="15">15%</option>
+                            <option value="20">20%</option>
+                        </select>
+                    </div>
+                    <div class="form__title-group">
+                        <button class="form__button--calc">Wylicz</button>
+                    </div>
+                </fieldset>
+            </form>	
+        </div>
 
+        <div class="overlay-container">
+            <div class="overlay-calc">
+                <div class="overlay-panel overlay-right">
+                    <?php
+                    if (isset($messages)) {
+                        if (count($messages) > 0) {
+                            echo '<ul style="margin: 20px; padding: 10px 10px 10px 30px; color: #fff; background-color: #860500; width:300px;">';
+                            echo 'Błędnie uzupełniony formularz!!! <br />';
+                            foreach ($messages as $key => $msg) {
+                                echo '<li>'.$msg.'</li>';
+                            }
+                            echo '</ul>';
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <?php if (isset($rate)){ ?>
+        <div style="margin: 20px; padding: 10px;  background-color: #021548;color: white; width:300px;">
+        <?php echo 'Miesięczna rata: '.round($rate, 2).' zł'; ?>
+        </div>
+        <?php } ?>
+    </div>
+    <a href="<?php print(_APP_ROOT);?>/app/security/logout.php" class="logout_button">Wyloguj się</a>
 </body>
 </html>
--- app/security/login_view.php
+++ app/security/login_view.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8" />
         <title>Logownanie</title>
 
-        <link rel="stylesheet" type="text/css" href="<?php print(_APP_ROOT); ?> /css/login_style.css" />
+        <link rel="stylesheet" type="text/css" href="<?php print(_APP_ROOT); ?> /css/style.css" />
         <script src="<?php print(_APP_ROOT); ?>/js/login_script.js" type="text/javascript"></script>
     </head>
     <body>
-        <div class="conteiner" id="conatiner" >
+        <div class="container" id="container" >
             <div class="form-container sign-in-container">
                 <form action="<?php print(_APP_ROOT);?> /app/security/login.php" method="post" class="form" id="login">
                 <h1 class="form__title">Logowanie</h1>
@@ -23,23 +23,24 @@
                     </div>
                     </fieldset>
                     <div class="form__title-group">
-                        <button type="submit" class="form__button">Zaloguj</button>
+                        <button class="form__button">Zaloguj</button>
                     </div>
                 </form>
             </div>
-        </div>
-    <div class="overlay-container">
-        <div class="overlay">
-            <div class="overlay-panel overlay-right">
-                <?php
-                    foreach($messeges as $key => $mes)
-                    {
-                        print('<span class="errorMessage">'.$mes.'</span>');
-                    }
-                ?>
+        
+            <div class="overlay-container">
+                <div class="overlay">
+                    <div class="overlay-panel overlay-right">
+                        <?php
+                            foreach($messeges as $key => $mes)
+                            {
+                                print('<span class="errorMessage">'.$mes.'</span>');
+                            }
+                        ?>
+                    </div>
                 </div>
+            </div>
         </div>
-    </div>
 
     </body>
 
